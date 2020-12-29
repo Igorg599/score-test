77f2f3a1de3b0c1c762a7bbcff6da009e9acf77f
Igor Gordeev <igorg599@gmail.com>
2020-12-29 12:45:52 +0500

    commit-3

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export {default as SortPopup} from './SortPopup';
 export {default as PizzaBlock} from './PizzaBlock';
 export {default as LoadingBlock} from './LoadingBlock';
+export {default as Pagination} from './Pagination';
+
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {fetchPizzas} from '../redux/actions/pizzas';
 import {setName, setSortBy} from '../redux/actions/filters';
-import {SortPopup, PizzaBlock, LoadingBlock} from '../components/';
+import {SortPopup, PizzaBlock, LoadingBlock, Pagination} from '../components/';
 
 const sortItems = [{name: 'популярности', type : 'popular', order: 'desc'},{name: 'ценe', type: 'price', order: 'desc'},{name: 'алфавиту', type: 'name', order: 'asc'}];
 
@@ -12,10 +12,16 @@ function Catalog() {
     const isLoaded = useSelector(({pizzas}) => pizzas.isLoaded);
     const {name, sortBy} = useSelector(({filters}) => filters);
     const [title, setTitle] = React.useState('');
+    const [currentPage, setCurrentPage] = React.useState(1); 
+    const [postsPerPage] = React.useState(6); 
 
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = items.slice(indexOfFirstPost, indexOfLastPost);
+ 
     React.useEffect(() => {
         dispatch(fetchPizzas(sortBy, name));
-    }, [name, sortBy]);
+    }, [name, sortBy, postsPerPage]);
 
     const onSelectSortType = React.useCallback((type) => {
         dispatch(setSortBy(type));
@@ -38,6 +44,8 @@ function Catalog() {
       setTitle(event.target.value.replace(/[^а-я]/g, ''));
       console.log(title);
     }
+    
+    const paginate = pageNumber => setCurrentPage(pageNumber);
 
     return(
       <div className="container">
@@ -59,13 +67,14 @@ function Catalog() {
           </form>
           <SortPopup activeSortType={sortBy.type} items={sortItems} onClickSortType={onSelectSortType}/>
         </div>
+        <Pagination postsPerPage={postsPerPage} totalPosts={items.length} paginate={paginate}/>
         <div className="content__items">
           {isLoaded ? 
-            items.map(obj => 
-            <PizzaBlock  
-            key={obj.id} 
-            {...obj}/>) : 
-            Array(12).fill(0).map((_, index) => <LoadingBlock key={index}/>)
+            currentPosts.map(obj => 
+              <PizzaBlock  
+              key={obj.id} 
+              {...obj}/>) : 
+            Array(3).fill(0).map((_, index) => <LoadingBlock key={index}/>)
           }
         </div>
       </div>
